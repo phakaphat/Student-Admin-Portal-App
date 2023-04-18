a5316f921a9452dec249b5c78c10cc02c81dae26
phakaphat <phakaphat.t@kkumail.com>
2023-04-18 17:20:14 +0700

    create interface StudentTable

---
+++ Student-Admin-Portal-App/src/app/interfaces/studentTable.ts
@@ -0,0 +1,12 @@
+export interface StudentTable {
+  id: number;
+  firstName: string;
+  lastName: string;
+  dateOfBtrth: number;
+  email: string;
+  mobile: number;
+  profileimageUrl: string;
+  genderId: number;
+  physicalAddress: string;
+  postalAddress: string;
+}
